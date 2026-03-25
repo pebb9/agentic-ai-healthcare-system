e66c8e21aafb017373a7c27ef084e94516fe4eb3
Pablo <pabloariasc2003@gmail.com>
2026-03-25 20:30:56 +0100

    feat: new deployment files to run app in claaudia

---
+++ claaudia/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+srun --mem=10G --cpus-per-task=8 --gres=gpu:1 --time=00:10:00 singularity exec --nv -B ../../agentic-ai-healthcare-system:/scratch/app -B ~/.cache/huggingface:/.cache/huggingface /ceph/container/pytorch/pytorch_26.02.sif bash /scratch/app/claaudia/run.sh
