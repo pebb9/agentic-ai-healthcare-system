8b1bd55ba6f53b48a2c391938b44e2d06e12f252
Pablo <pabloariasc2003@gmail.com>
2026-04-10 00:01:28 +0200

    fix: only mcp running medgemma open-weigths model

--- claaudia/run.sh
+++ claaudia/run.sh
@@ -6,5 +6,15 @@ cd /scratch/app
 echo -e "\nInstalling python requirements\n"
 pip install --no-cache-dir -r requirements.txt
 
+echo -e "\nStarting MCP server\n"
+python3 mcp_server.py &
+MCP_PID=$!
+
+# Little sleep to let mcp_server start up properly before running main app
+sleep 7
+
 echo -e "\nRunning app\n"
 python3 main.py
+
+# Clean MCP server after main app finishes
+kill $MCP_PID
