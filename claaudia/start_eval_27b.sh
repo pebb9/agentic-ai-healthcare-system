790e29ac4b40af282162ecde2a7a2ab4fc26cec4
Rong-student <vnguye25@student.aau.dk>
2026-05-11 16:12:42 +0200

    add a script to run srun

---
+++ claaudia/start_eval_27b.sh
@@ -0,0 +1,1 @@
+srun --mem=58G --cpus-per-task=8 --gres=gpu:3 --time=00:30:00 singularity exec --nv -B ../../agentic-ai-healthcare-system:/scratch/app -B ~/.cache/huggingface:/.cache/huggingface /ceph/container/pytorch/pytorch_26.02.sif bash /scratch/app/claaudia/run_evaluation.sh
