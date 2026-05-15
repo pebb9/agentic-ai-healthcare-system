0d4599e65623f68f6f505b930860e3f769059240
pebb9 <pabloariasc2003@gmail.com>
2026-05-15 21:46:24 +0200

    fix: rename testing sbatch files

--- claaudia/start_testing.sh
+++ claaudia/start_testing.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-#SBATCH --job-name=medgemma-evaluation
-#SBATCH --output=evaluation-logs/medgemma-evaluation.out
-#SBATCH --error=evaluation-logs/medgemma-evaluation.err
+#SBATCH --job-name=medgemma-testing
+#SBATCH --output=testing-logs/medgemma-testing.out
+#SBATCH --error=testing-logs/medgemma-testing.err
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=58G
 #SBATCH --gres=gpu:3
